d191f87898ffd15e50938bc35439f07806af7544
Muhalat <muhalat17@gmail.com>
2022-10-13 13:39:54 +0100

    Project DONE

--- js/app.js
+++ js/app.js
@@ -22,8 +22,10 @@
  * Define Global Variables
  * 
 */
-const sections = document.getElementsByTagName('section')
+const sections = document.querySelectorAll('section')
 
+const navbarList = document.getElementById('navbar__list')
+const menuSection = ['Section 1','Section 2','Section 3','form-section']
 /**
  * End Global Variables
  * Start Helper Functions
@@ -38,18 +40,33 @@ const sections = document.getElementsByTagName('section')
 */
 
 // build the nav
-const nav = document.getElementById('navbar__list');
-nav.innerHTML='<li> <a href="#">Home</a> </li> <li> <a href="#">About</a> </li> <li> <a href="blog-post.html">Gallery</a> </li> <li> <a href="#">Contact</a> </li>'
 
-nav.addEventListener('keypress', function(){
-    console.log("stop touching me!!!!")
-})
+// let newNavItem = document.createElement('li')
+// newNavItem.innerHTML='<li><a href="#section1">Section 1</a></li> <li> <a href="#section2">Section 2</a> </li> <li> <a href="#section3">Section 3</a> </li> <li> <a href="#form-section">Section 4</a> </li>'
+// navbarList.appendChild(newNavItem)
 // Add class 'active' to section when near top of viewport
-
-
+function menu(){
+    for(let i=0; i<sections.length; i++){
+        const List = document.createElement('li')
+        const anchor = document.createElement('a')
+        anchor.textContent = menuSection[i]
+        anchor.setAttribute('class','link')
+        anchor.setAttribute('href',`#${sections[i].id}`)
+       navbarList.appendChild(List)
+       List.appendChild(anchor)
+    }
+}
+menu();
 // Scroll to anchor ID using scrollTO event
-
-
+function anchorScroll(){
+    navbarList.addEventListener('click',(e)=>{
+        e.preventDefault()
+        const targetLink = e.target
+        const targetSection = document.querySelector(targetLink.getAttribute('href'))
+        targetSection && targetSection.scrollIntoView({behavior : 'smooth'})
+})
+}
+anchorScroll();
 
 /**
  * End Main Functions
@@ -59,30 +76,52 @@ nav.addEventListener('keypress', function(){
 
 // Build menu 
 
-// Scroll to section on link click
-nav.addEventListener('click',function(){
-    window.scrollTo( {top: 100,
-        left: 100,
-        behavior: 'smooth'})
-})
 // Set sections as active
 function makeActive(){
-    for (const section of sections) {
+    sections.forEach((section) => {
+        window.addEventListener("scroll",  ()=>{
         const box = section.getBoundingClientRect();
-        if (box.top <= 250 && box.bottom >= 0) {
-        console.log ("activeState");
-        } else {
-            console.log("inactive");
+        const currentSection = section.getAttribute('id')
+        const navLink = document.querySelector(`[href='#${currentSection}']`)
+        if (box.top <= 100 && box.bottom >= 0) {
+           
+            navLink.classList.add('active')
+        } 
+        else{
+            navLink.classList.remove('active')
         }
-     }
-  }
+    })
+  })
+}
+makeActive();
+ 
+// document.addEventListener('scroll', ()=>{
+//     let current = '';
+//     sections.forEach(section =>{
+//         const sectionTop = section.offsetTop;
+//         const sectionHeight = section.clientHeight;
+//         if(scrollY >= sectionTop - sectionHeight / 3){
+//             current = section.getAttribute('id');
+//         }
+//     })
+    // navbarList.forEach(li =>{
+    //     li.classList.remove('active')
+    //     if(li.classList.contains(current)){
+    //         li.classList.add('active')
+    //     }
+    // })
+// })
 
-document.addEventListener("scroll", function() { makeActive();});
 //Form submission success
-const done = document.getElementById('submit');
-done.addEventListener('click', function(){
-    document.getElementById('passwordalert').innerHTML='<span style="color:green">Submitted successfully!</span>'
-    
+   const form = document.querySelector('form')
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    // document.getElementById('passwordalert').innerHTML='<span style="color:green">Submitted successfully!</span>'
+    const success = document.createElement('p')
+    success.textContent = 'Submitted successfully!'
+    form.appendChild(success)
+    form.reset()
+    setTimeout(() => {
+        success.remove()
+    }, 3000)
 })
-
-
